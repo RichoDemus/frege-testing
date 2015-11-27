ba24a9d7645038c5c7a86ef5be59729aec1b9ca1
RichoDemus <richard.tjerngren@office.necorp.dom>
2015-11-27 16:35:58 +0100

    Cleaned away all the test-stuff from the original repo

--- src/test/java/com/talios/frege/AppTest.java
+++ src/test/java/com/talios/frege/AppTest.java
@@ -1,45 +1,11 @@
 package com.talios.frege;
 
-import frege.prelude.PreludeBase;
 import org.testng.annotations.Test;
-
-import static com.google.common.truth.Truth.assertThat;
-import static com.talios.frege.Greet.TField;
-import static com.talios.frege.Greet.TGreeting;
-import static com.talios.frege.Greet.TSalutation;
 
 public class AppTest {
 
     @Test
-    public void testDataType() {
-
-        TGreeting newGreeting  = TGreeting.mk("Mark", TSalutation.mk("Hello there", "Chow!"));
-
-        System.out.println(MyFrege.testGreeting);
-
-        assertThat(TGreeting.name(newGreeting)).isEqualTo("Mark");
-        assertThat(TSalutation.welcome(TGreeting.salutation(newGreeting))).isEqualTo("Hello there");
-
-        String greet = MyFrege.makeGreeting(newGreeting);
-        String farewell = MyFrege.makeFarewell(newGreeting);
-
-        System.out.println(farewell);
-
-        PreludeBase.TST.performUnsafe(MyFrege.greet(newGreeting));
-
-        assertThat(greet).isEqualTo("Hello there Mark, how are you?");
-
-        // create a java class and pass that to our frege function
-        MyJavaClass mjc = com.talios.frege.Testing.makeHim("Made in Java, via Frege");
-        System.out.println("His name is: " + mjc.getName());
-
-        TField field = TField.DStandardField.mk("Mark", "title", "fieldType");
-
-        System.out.println("field is: " + Greet.IShow_Field.show(field));
-        System.out.println("name is: " + TField.M.name(field));
-
-        com.talios.frege.Testing.main(new String[] {""});
-        com.talios.frege.MyFrege.main(new String[] {""});
+    public void emptyTest() {
     }
 
 }
